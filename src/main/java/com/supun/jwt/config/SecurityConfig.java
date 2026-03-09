60c863b3d92c19c0b514a2df0bd08c334bb0ae7e
supun-pregeeth <ruchiraniasanka@gmail.com>
2026-03-09 14:31:35 +0530

    add comment of security config

--- src/main/java/com/supun/jwt/config/SecurityConfig.java
+++ src/main/java/com/supun/jwt/config/SecurityConfig.java
@@ -8,21 +8,27 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
-@Configuration
+@Configuration //This class contains configuration settings for the application
 public class SecurityConfig {
 
+    //this is the guard
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //this defines the security pipeline for every request.
+    //Request → SecurityFilterChain → Controller
+    public SecurityFilterChain securityFilterChain(
+            @org.jetbrains.annotations.NotNull HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())//Disable CSRF- it is used mainly for web. But when using JWT APIs, we usually disable it.
+                //This defines who can enter which room.
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+
+        return http.build();//Okay, the security rules are ready. Activate them.
     }
 }
